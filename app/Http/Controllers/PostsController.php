a1b17200d0a0f19f77511e51ad691097c0b78705
MahmudJion <43998247+MahmudJion@users.noreply.github.com>
2020-10-19 22:13:54 +0600

    init setup

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function save(Request $request)
+    {
+        // set form validation rules
+        $this->validate($request, [
+            'title' => 'required',
+            'body'    => 'required'
+        ]);
+
+        // if the validation passes, save to database and redirect
+    }
+}
---
+++ resources/assests/js/app.js
@@ -0,0 +1,56 @@
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+
+// tell Vue to use the vue-resource plugin
+Vue.use(VueResource);
+
+// import FormError component
+import FormError from './components/FormError.vue';
+
+// get csrf token
+Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#token').getAttribute('value');
+
+// instantiate a new Vue instance
+new Vue({
+    // mount Vue to .container
+    el: '.container',
+
+   // define components
+    components: {
+    	FormError,
+    },
+
+    data: {
+    	post: {
+    		title: '',
+    		body: '',
+    	},
+
+    	submitted: false,
+
+	    // array to hold form errors
+    	errors: [],
+    },
+
+    methods: {
+    	createPost() {
+			let post = this.post;
+
+			this.$http.post('create-post', post).then(function(response) {
+				// form submission successful, reset post data and set submitted to true
+				this.post = {
+					title: '',
+					body: '',
+				};
+
+                // clear previous form errors
+                this.$set('errors', '');
+
+				this.submitted = true;
+			}, function(response) {
+			    // form submission failed, pass form  errors to errors array
+				this.$set('errors', response.data);
+			});
+		}
+    }
+});
